cd29f5f596c06b6da7184e462b41591bad6428d1
Young-Pastor <young@zhisida.com>
2022-07-10 17:39:42 +0800

    【新增】新增事件分析实现

--- src/components/Charts/MiniBar.vue
+++ src/components/Charts/MiniBar.vue
@@ -8,14 +8,13 @@
 </template>
 
 <script>
-  import moment from 'dayjs'
 
   const sourceData = []
   const beginDay = new Date().getTime()
 
   for (let i = 0; i < 10; i++) {
     sourceData.push({
-      x: moment(new Date(beginDay + 1000 * 60 * 60 * 24 * i)).format('YYYY-MM-DD'),
+      x: '',
       y: Math.round(Math.random() * 10)
     })
   }
