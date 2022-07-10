dc41547b69373336423211dfb80897582febd3cb
Young-Pastor <young@zhisida.com>
2022-07-10 17:51:14 +0800

    【新增】新增事件分析实现

--- src/components/Charts/LineChartMultid.vue
+++ src/components/Charts/LineChartMultid.vue
@@ -26,26 +26,13 @@
       dataSource: {
         type: Array,
         default: () => [
-          { type: 'Jan', [1]: 7.0,  [2]: 3.9  },
-          { type: 'Feb', [1]: 6.9,  [2]: 4.2  },
-          { type: 'Mar', [1]: 9.5,  [2]: 5.7  },
-          { type: 'Apr', [1]: 14.5, [2]: 8.5  },
-          { type: 'May', [1]: 18.4, [2]: 11.9 },
-          { type: 'Jun', [1]: 21.5, [2]: 15.2 },
-          { type: 'Jul', [1]: 25.2, [2]: 17.0 },
-          { type: 'Aug', [1]: 26.5, [2]: 16.6 },
-          { type: 'Sep', [1]: 23.3, [2]: 14.2 },
-          { type: 'Oct', [1]: 18.3, [2]: 10.3 },
-          { type: 'Nov', [1]: 13.9, [2]: 6.6  },
-          { type: 'Dec', [1]: 9.6,  [2]: 4.8  }
         ]
       },
       fields: {
         type: Array,
-        default: () => [[1], [2]]
+        default: () => []
       },
-      // 别名，需要的格式：[{field:'name',alias:'姓名'}, {field:'sex',alias:'性别'}]
-      aliases:{
+      aliases: {
         type: Array,
         default: () => []
       },
@@ -74,10 +61,10 @@
           key: 'x',
           value: 'y'
         })
-        let rows =  dv.rows
+        const rows = dv.rows
         // 替换别名
         rows.forEach(row => {
-          for (let item of this.aliases) {
+          for (const item of this.aliases) {
             if (item.field === row.x) {
               row.x = item.alias
               break
@@ -89,7 +76,3 @@
     }
   }
 </script>
-
-<style scoped>
-
-</style>
